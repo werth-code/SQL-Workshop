f8563bad15eb45243791fce555c70bb44237ef60
Matt Werth <matthewwerth@gmail.com>
2021-01-04 20:50:25 -0500

    Added Basic Setup For Connection To Database

--- src/main/java/werth/matt/App.java
+++ src/main/java/werth/matt/App.java
@@ -1,2 +1,28 @@
-package werth.matt;public class App {
+package werth.matt;
+
+import java.sql.*;
+import java.util.UUID;
+
+// TODO: 1/4/21 This connects us to a docker container we set up with mysql on port 3306
+
+public class App {
+
+    public void startConnection() {
+        try{
+            Connection connection = DriverManager
+                    .getConnection("jdbc:mysql://localhost:3306/cdDb?createDatabaseIfNotExist=true&useSSL=false", "developer01", "pass");
+            System.out.println("Successful Connection!");
+            connection.setAutoCommit(false);
+        }
+        catch (Exception e) {
+            System.out.println("FAILED CONNECTION! " + e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        App app = new App();
+        app.startConnection();
+    }
+
+
 }
